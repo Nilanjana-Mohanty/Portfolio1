8a13f3c9a0999683c009b7ce1d63be1d16fead77
Nilanjana-Mohanty <nilanjana.m3003@gmail.com>
2023-10-20 20:02:37 +0530

    dark mode and transitions added

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const toggleBtn = document.querySelector('.toggle_btn');
 const toggleBtnIcon = document.querySelector('.toggle_btn i');
 const dropMenu = document.querySelector('.navbar');
 
+var icon = document.querySelector(".icon");
+var iconBtn = document.querySelector('.icon i');
+
 function toggledrop(){
     dropMenu.classList.toggle('open')
     const isOpen =dropMenu.classList.contains('open')
@@ -9,5 +12,18 @@ function toggledrop(){
     toggleBtnIcon.classList = isOpen
         ? 'ri-arrow-up-s-line'
         : 'ri-menu-line'
+}
+
+function theme(){
+
+    document.body.classList.toggle("darkTheme");
+
+    const isOn =document.body.classList.contains("darkTheme")
+
+    iconBtn.classList = isOn
+        ?   'ri-moon-fill'
+        :   'ri-sun-fill'
+
+
 }
 
